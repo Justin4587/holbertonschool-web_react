36c1ac27c9dbcd6ee5976cf2f3542aec5b66e819
Justin Thurman <1828@holbertonschool.com>
2022-05-14 21:08:52 -0500

    I get the correct table to display but no idea how to fix the type Errors, need to move on hopefully it will become more obvious.

---
+++ 0x00-TypeScript/task_0/js/main.ts
@@ -0,0 +1,51 @@
+interface Student {
+  firstName: string;
+  lastName: string;
+  age: number;
+  location: string;
+}
+
+const student1: Student = {
+  firstName: 'Buddy',
+  lastName: 'Turd',
+  age: 15,
+  location: 'byMe',
+};
+
+const student2: Student = {
+  firstName: 'new',
+  lastName: 'me',
+  age: 40,
+  location: 'Texas',
+
+};
+
+const studentsList: Array<Student> = [student1, student2];
+
+const table: HTMLTableElement = document.createElement('table');
+const thead: HTMLTableSectionElement = document.createElement('thead');
+const tbody: HTMLTableSectionElement = document.createElement('tbody');
+const headers: string[] = ['firstName', 'location'];
+
+
+
+const tableHead: HTMLTableRowElement = thead.insertRow(0);
+const cell1: HTMLTableCellElement = tableHead.insertCell(0);
+cell1.innerHTML = headers[0];
+const cell2: HTMLTableCellElement = tableHead.insertCell(1);
+cell2.innerHTML = headers[1];
+
+document.body.appendChild(table);
+table.append(thead);
+table.append(tbody);
+
+studentsList.forEach((student) => {
+  const row: HTMLTableRowElement = tbody.insertRow(0);
+  const cell1: HTMLTableCellElement = row.insertCell(0);
+  const cell2: HTMLTableCellElement = row.insertCell(1);
+  cell1.innerHTML = student.firstName;
+  cell2.innerHTML = student.location;
+});
+
+
+
