b17b665a0dde4ccb96ab653aa23b234436d06ffa
Justin Thurman <1828@holbertonschool.com>
2022-05-27 22:46:34 -0500

    again11

--- 0x01-Webpack/task_2/webpack.config.js
+++ 0x01-Webpack/task_2/webpack.config.js
@@ -2,11 +2,11 @@ const path = require("path");
 
 
 module.exports = {
-  entry: "./js/dashboard_main.js",
+  entry: './js/dashboard_main.js',
   mode: 'production',
   output: {
-    filename: "bundle.js",
-    path: path.resolve(__dirname, "public")
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'public')
   },
   module: {
     rules: [
