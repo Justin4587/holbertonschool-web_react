1eed85d22e513a15a3d413c12fa93cb419845175
Justin Thurman <1828@holbertonschool.com>
2022-06-24 09:56:13 -0500

    public to ./public

--- 0x01-Webpack/task_2/webpack.config.js
+++ 0x01-Webpack/task_2/webpack.config.js
@@ -8,7 +8,7 @@ module.exports = {
   mode: 'production',
   output: {
     filename: 'bundle.js',
-    path: path.resolve(__dirname, 'public')
+    path: path.resolve(__dirname, './public')
   },
   module: {
     rules: [
