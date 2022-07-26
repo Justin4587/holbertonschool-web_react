88e9ce61ec677f6a9f5fd73d33f55bb46197ee94
Justin Thurman <1828@holbertonschool.com>
2022-07-26 15:54:03 -0500

    #4 looks ok except the oliver part doesn't make sense, change it from 4 to 4?

---
+++ 0x07-React_Immutable/4-mutations.js
@@ -0,0 +1,19 @@
+import { Map } from 'immutable';
+// import pkg from 'immutable';
+
+// const { List } = pkg;
+
+export const map = Map(
+   {
+    1: 'Liam',
+    2: 'Noah',
+    3: 'Elijah',
+    4: 'Oliver',
+    5: 'Jacob',
+    6: 'Lucas',
+  }
+);
+
+export const map2 = map.withMutations((map) => {
+  map.set(2, 'Benjamin').set(4, 'Oliver');
+});
