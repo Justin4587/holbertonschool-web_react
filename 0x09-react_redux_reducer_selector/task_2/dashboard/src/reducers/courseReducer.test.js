6f4b985287ef6cd9f21f77bbe71daaa23fe048a2
Justin Thurman <1828@holbertonschool.com>
2022-08-03 22:50:01 -0500

    finally done (I'm pretty sure) with task_2   Not sure if Ill have the energy for task_3 tonight.

---
+++ 0x09-react_redux_reducer_selector/task_2/dashboard/src/reducers/courseReducer.test.js
@@ -0,0 +1,67 @@
+
+import courseReducer from './courseReducer';
+import { fetchCourseSuccess, selectCourse, unSelectCourse } from '../actions/courseActionCreators';
+
+const falseList = [
+  {
+    id: 1,
+    name: "ES6",
+    isSelected: false,
+    credit: 60
+  },
+  {
+    id: 2,
+    name: "Webpack",
+    isSelected: false,
+    credit: 20
+  },
+  {
+    id: 3,
+    name: "React",
+    isSelected: false,
+    credit: 40
+  }
+];
+
+const oneTrueList = [
+  {
+    id: 1,
+    name: "ES6",
+    isSelected: true,
+    credit: 60
+  },
+  {
+    id: 2,
+    name: "Webpack",
+    isSelected: false,
+    credit: 20
+  },
+  {
+    id: 3,
+    name: "React",
+    isSelected: false,
+    credit: 40
+  }
+];
+
+describe('courseReducer', () => {
+  it('verifies default state returns empty array', () => {
+    const res = courseReducer(undefined, 'no_action');
+    expect(res).toEqual([]);
+  });
+
+  it('verifies fetchCourseSuccess returns data passed', () => {
+    const res = courseReducer([], fetchCourseSuccess(oneTrueList));
+    expect(res).toEqual(falseList);
+  });
+
+  it('verifies selectCourse returns data with isSelected as true', () => {
+    const res = courseReducer(falseList, selectCourse(1));
+    expect(res).toEqual(oneTrueList);
+  });
+
+  it('verifies unSelectCourse returns data with isSelected as false', () => {
+    const res = courseReducer(oneTrueList, unSelectCourse(1));
+    expect(res).toEqual(falseList);
+  });
+});
