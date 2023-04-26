bf5ac8f8cdf6c5050385b3e1da956213c7add530
Justin Thurman <1828@holbertonschool.com>
2023-04-25 20:26:37 -0500

    reworking GHPages

---
+++ 0x08_react_redux_action_creator_normalizr/GHPages/dashboard/src/actions/notificationActionTypes.js
@@ -0,0 +1,13 @@
+export const MARK_AS_READ = {
+  type: 'MARK_AS_READ',
+  index: ''
+}
+export const SET_TYPE_FILTER = {
+  type: 'SET_TYPE_FILTER',
+  filter: ''
+};
+
+export const NotificationTypeFilters = {
+  DEFAULT: 'DEFAULT',
+  URGENT: 'URGENT'
+}
---
+++ 0x08_react_redux_action_creator_normalizr/GHPages/dashboard/src/actions/uiActionsTypes.js
@@ -0,0 +1,33 @@
+export const LOGIN = {
+  type: 'LOGIN',
+  user: {
+    email: "",
+    password: ""
+  } 
+}
+export const LOGOUT = {
+  type: 'LOGOUT',
+  user: {
+    email: "",
+    password: ""
+  }
+}
+
+export const DISPLAY_NOTIFICATION_DRAWER = {
+  type: 'DISPLAY_NOTIFICATION_DRAWER',
+  user: {
+    email: "",
+    password: ""
+  }
+}
+
+export const HIDE_NOTIFICATION_DRAWER = {
+  type: 'HIDE_NOTIFICATION_DRAWER',
+  user: {
+    email: "",
+    password: ""
+  }
+}
+
+export const LOGIN_SUCCESS = 'LOGIN_SUCCESS';
+export const LOGIN_FAILURE = 'LOGIN_FAILURE';
